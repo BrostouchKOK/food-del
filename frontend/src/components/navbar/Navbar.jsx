87da5c90f25f7358df67e3b5fc3ad710007bb0a1
Brostouch KOK <brostouchkokit1007@gmail.com>
2025-05-28 17:49:27 +0700

    custom navabr menu

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,30 +1,28 @@
-import React from 'react'
-import './Navbar.css';
-import {assets} from "../../assets/frontend_assets/assets"
+import React, { useState } from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/frontend_assets/assets";
 const Navbar = () => {
+  const [menu, setMenu] = useState("menu");
+
   return (
-    <div className='navbar'>
-      <h1 className='logo'>The-Fast.</h1>
-      <ul className='navbar-menu'>
-        <li>
-            <a href="">home</a>
-        </li>
-        <li>
-            <a href="">about</a>
-        </li>
-        <li><a href="">mobile-app</a></li>
-        <li>
-            <a href="">contact</a>
-        </li>
+    <div className="navbar">
+      <h1 className="logo">The-Fast.</h1>
+      <ul className="navbar-menu">
+        <li onClick={()=>setMenu("home")} className={menu === "home"?"active" : ""}>home</li>
+        <li onClick={()=>setMenu("menu")} className={menu === "menu"?"active" : ""}>menu</li>
+        <li onClick={()=>setMenu("mobile-app")} className={menu === "mobile-app"?"active" : ""}>mobile-app</li>
+        <li onClick={()=>setMenu("contact")} className={menu === "contact"?"active" : ""}>contact</li>
       </ul>
-      <div className='navbar-right'>
+      <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
-        <div className='navbar-search-icon'>
-            <img src={assets.basket_icon} alt="" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
         </div>
+        <button>sign up</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
